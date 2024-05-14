2ef11e2aa9c9af9269212db66a7ec025993cc19c
Craig MacGregor <cmacgregor.uk@gmail.com>
2024-05-14 12:26:24 +0100

    Submitting new notes logic

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,84 +1,103 @@
 import './App.css';
 import {useState} from "react";
+import React from "react";
 
 type Note = {
   id: number;
   title: string;
   content: string;
 }
-let newContent = "hello world"
-let newHeader = "new note"
-let x = 5;
 
-
+let x = "Thursday"
 
 const App = () => {
-  
+  // Note[] states that we use type note, making sure elements are number, string twice
+  // Allows us to easily see what components
   const [notes, setNotes] = useState<Note[]>([
     {
       id: 1,
       title: "note title 1",
-      content: "Hello this is my first note"
+      content: "girlfriend is the best"
     },
     {
       id: 2,
-      title: "Thursday",
-      content: "It was a very nice day"
+      title: x,
+      content: "girlfriend loves you"
     },
     {
       id: 3,
       title: "Saturday",
-      content: "It wasn't such a nice day"
+      content: "girlfriend is very proud of you"
     },
-    {
-      id: 4,
-      title: newHeader,
-      content: newContent
-    }
   ]);
-  function addNote(){
-    console.log("button clicked");
-    const inputElement = document.getElementById("noteInput") as HTMLInputElement;
-    const inputHeader = document.getElementById("noteHeader") as HTMLInputElement;
-  
-    const content = inputElement.value;
-    const header = inputHeader.value;
-  
-    if(content.trim() !== "" && header.trim() !==""){
-      
-      notes[3].content=content;
-      notes[3].title=header;
-    }
-  }
-  /*document.addEventListener("DOMCOontentLoaded", function(){
-    const mybutton = document.getElementById("submit");
 
-    mybutton?.addEventListener("click", addNote)
-  });*/
+  const [ title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+
+  /*function handleSubmit (event:React.FormEvent){
+    const inputElement = document.getElementById("noteInput") as HTMLInputElement;
+    const inputHeader = document.getElementById("noteTitle") as HTMLInputElement;
+    const title = inputHeader.value;
+    const content = inputElement.value;
+    let Note = {3: 4, header, content};
+    notes.push(Note);
+  }*/
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("title: " , title);
+    console.log("content:", content);
+    // Object of type Note
+    const newNote: Note = {
+      id: notes.length+1,
+      title: title,
+      content: content
+    }
+
+    setNotes([newNote, ...notes]);
+    setTitle("");
+    setContent("");
+
+  }
   return(
     <div className="app-container">
       {/* Input for note */}
-      <form className = "note-form">
+      <form 
+        className = "note-form"
+        onSubmit ={(event) => handleSubmit(event)}
+        >
         <input 
-          placeholder = "title" 
+          placeholder = "Title" 
+          value = {title}
+          // Changes the title state value based on the user input
+          onChange={(event)=>
+            setTitle(event.target.value)
+          }
           id="noteTitle"
           required>  
         </input>
         <textarea 
           placeholder="Content"
+          value = {content}
+          onChange={(event)=>
+            setContent(event.target.value)
+          }
           id="noteInput"
           rows = {10}
           required>  
         </textarea>
         <button
           id="submit"
+          onClick={handleSubmit}
           >
           Add Note
         </button>
+
+
       </form>
       <div className = "notes-grid">
         {notes.map((note)=>(
-          <div className = "note-item">
+          <div className = "note-item" key={note.id}>
             <div className ="notes-header">
               <button><img src="images/x.png" alt="x"></img></button>
             </div>
