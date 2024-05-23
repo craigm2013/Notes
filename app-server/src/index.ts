29ea780c8a50141291618730dc8cb56fecdbff24
Craig MacGregor <cmacgregor.uk@gmail.com>
2024-05-23 17:25:09 +0100

    Database Connected

--- app-server/src/index.ts
+++ app-server/src/index.ts
@@ -6,7 +6,13 @@ const app = express();
 app.use(express.json());
 app.use(cors())
 
+// Our ui will call to get all of our notes, 
+
 // Chosen port of 5000
 app.listen(5000, () => {
-    console.log("server running on localhost:5000")
+    console.log("server running on localhost:5000" )
 })
+
+app.get("/api/notes", async(req, res) =>{
+    res.json({message:"success!"});
+});
